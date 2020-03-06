86fff7a077068d0ba03599602f8fb1bc1f572147
chenfeiyu <chenfeiyu@baidu.com>
2020-03-06 02:55:42 +0000

    add doc for parakeet.data, python2 compatability for DataIterator and lazy CacheDataset

--- parakeet/utils/layer_tools.py
+++ parakeet/utils/layer_tools.py
@@ -36,14 +36,3 @@ def freeze(layer):
 def unfreeze(layer):
     for param in layer.parameters():
         param.trainable = True
-
-
-def torch_summary(layer):
-    num_params = num_elements = 0
-    print("layer summary:")
-    for name, param in layer.named_parameters():
-        print("{}|{}|{}".format(name, param.shape, np.prod(param.shape)))
-        num_elements += np.prod(param.shape)
-        num_params += 1
-    print("layer has {} parameters, {} elements.".format(num_params,
-                                                         num_elements))
